1f73a2bd1e3a4c354c94e57ee392baff9b31874e
Fanny Arlesit <fanny.arles@orange.fr>
2025-12-03 11:40:09 +0100

    debug mongo

--- test_db.js
+++
@@ -1,8 +0,0 @@
-// test.js
-const mongoose = require('mongoose');
-
-const uri = "mongodb+srv://admin:kFXS8inlr9JOG40g6Rxc@mongodb-615f9f70-o2c747902.30e4c693.database.cloud.ovh.net/admin?replicaSet=replicaset&tls=true";
-
-mongoose.connect(uri)
-  .then(() => console.log("✅ Connecté !"))
-  .catch(err => console.error("❌ Erreur :", err));
