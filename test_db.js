9bd224d5c4dbf70de2c8265ec9e9e1b44717907a
Fanny Arlesit <fanny.arles@orange.fr>
2025-12-03 11:30:09 +0100

    mongo debug

--- test_db.js
+++ test_db.js
@@ -1,7 +1,7 @@
 // test.js
 const mongoose = require('mongoose');
 
-const uri = "mongodb+srv://fannyarles.design+apiculture@gmail.com:*Fanny97415228@mongodb-615f9f70-o2c747902.30e4c693.database.cloud.ovh.net/admin?replicaSet=replicaset&tls=true";
+const uri = "mongodb+srv://admin:kFXS8inlr9JOG40g6Rxc@mongodb-615f9f70-o2c747902.30e4c693.database.cloud.ovh.net/admin?replicaSet=replicaset&tls=true";
 
 mongoose.connect(uri)
   .then(() => console.log("✅ Connecté !"))
